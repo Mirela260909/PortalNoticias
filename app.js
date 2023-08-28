7db03e0264e8124ad309233c28f35d6a7a521b06
Mirela260909 <mirelanakano@gmail.com>
2023-08-28 16:19:44 -0400

    crie um teste

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = require('./config/server.js');
+
+var rotaHome=require('./app/routes/home.js')(app);
+
+var rotaNoticias=require('./app/routes/noticias.js')(app);
+
+var rotaForm_add_noticia=require('./app/routes/form_add_noticia.js')(app);
+
+
+
+
+
+
+app.listen(3000, function(){ 
+	console.log('Servidor rodando com Express');
+});
---
+++ app/routes/noticias.js
@@ -0,0 +1,27 @@
+module.exports=function(app){
+
+app.get('/noticias', function(req,res){ 
+
+	var mysql=require('mysql');
+	var connection=mysql.createConnection(
+		{
+			host: 'localhost',
+			user: 'root',
+			password: 'root',
+			database: 'portal_noticias'
+		}
+	);
+
+	connection.query('select * from noticias',function(erro,result){
+		if (erro){
+			console.log(erro);
+		}
+	res.send(result);
+
+})
+
+		
+});
+
+	
+}
